0565981f1e671d903ecd1fbe4ab0bf8f7412285b
loui9583 <loui9583@stud.kea.dk>
2024-02-08 23:39:11 +0100

    added exercises 3.Objects and 4.Arrays

---
+++ 02._Intro_Exercises/3.Objects.js
@@ -0,0 +1,44 @@
+// --------------------------------------
+// Objects
+// --------------------------------------
+// Exercise 1 - Retrieve value from object by key
+
+const myObject = {message: "Hello, earthling! I bring peace."};
+
+// Log the message
+
+console.log(myObject.message);
+
+// --------------------------------------
+// Exercise 2 - Defining an object.
+
+const person = {name: "Louis Nordsø", age: 29}
+
+// Create an object that has your name and age.
+
+
+// --------------------------------------
+// Exercise 3 - Add a key-value pair
+
+const stackOverflow = {};
+
+stackOverflow.exampleKey = "example value";
+stackOverflow.isAllowed = true;
+
+// make a rule called isAllowed and let the value be true
+
+console.log(stackOverflow);
+// --------------------------------------
+// Exercise 4 - Remove a property
+
+const thisSong = {description: "The best song in the world."};
+
+delete thisSong.description;
+thisSong.about = "Just a tribute"
+
+console.log(thisSong);
+// remove the property "description" and add a property called "about" that should say "Just a tribute."
+
+
+// --------------------------------------
+
